4b59a0b1ce88fc7b801158015641349e733fbddf
chaudhari-aditya03 <adityachaudhariedu312005@gmail.com>
2025-07-30 15:43:50 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Scroll reveal animations
+ScrollReveal().reveal('#chairman', { delay: 200, distance: '50px', origin: 'bottom' });
+ScrollReveal().reveal('#principal', { delay: 400, distance: '50px', origin: 'bottom' });
+ScrollReveal().reveal('#history', { delay: 600, distance: '50px', origin: 'bottom' });
+ScrollReveal().reveal('#programs', { delay: 800, distance: '50px', origin: 'bottom' });
